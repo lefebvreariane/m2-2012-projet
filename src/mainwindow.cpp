3352a10875b34108daa285ef07b293b6de1041ea
lefebvreariane@gmail.com <lefebvreariane@gmail.com@e3157d87-c1cc-ae76-3296-7ee3db5a2ebd>
2012-12-20 08:56:24 +0000

    code interface a jour

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
+
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->centralWidget();
 }
 
 MainWindow::~MainWindow()
@@ -15,6 +19,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::changeEvent(QEvent *e)
 {
+    cout << "coucou1" << endl;
     QMainWindow::changeEvent(e);
     switch (e->type()) {
     case QEvent::LanguageChange:
@@ -27,14 +32,27 @@ void MainWindow::changeEvent(QEvent *e)
 
 void MainWindow::on_actionExit_triggered()
 {
-    close();
-}
-
-void MainWindow::on_tabWidget_tabCloseRequested(int index)
-{
-    //if (textEdit->isWindowModified()){
-         QMessageBox *popupSave = new QMessageBox();
-         popupSave->setVisible(true);
-    //}
-    //tabWidget->removeTab(index);
+    QMessageBox *popupSave = new QMessageBox();
+    popupSave->setVisible(true);
+    popupSave->setText("The document has been modified.");
+    popupSave->setInformativeText("Do you want to save your changes?");
+    popupSave->setStandardButtons(QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
+    popupSave->setDefaultButton(QMessageBox::Save);
+    int ret = popupSave->exec();
+    switch (ret) {
+    case QMessageBox::Save:
+        // Save was clicked
+        close();
+        break;
+    case QMessageBox::Discard:
+        // Don't Save was clicked
+        close();
+        break;
+    case QMessageBox::Cancel:
+        // Cancel was clicked
+        break;
+    default:
+        // should never be reached
+        break;
+    }
 }
--- src/moc_mainwindow.cpp
+++ src/moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Sun Dec 16 17:35:40 2012
+** Created: Wed Dec 19 15:48:52 2012
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
 **
 ** WARNING! All changes made in this file will be lost!
@@ -23,7 +23,7 @@ static const uint qt_meta_data_MainWindow[] = {
        4,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       1,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -31,15 +31,13 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: signature, parameters, type, tag, flags
-      18,   12,   11,   11, 0x08,
-      54,   11,   11,   11, 0x08,
+      12,   11,   11,   11, 0x08,
 
        0        // eod
 };
 
 static const char qt_meta_stringdata_MainWindow[] = {
-    "MainWindow\0\0index\0on_tabWidget_tabCloseRequested(int)\0"
-    "on_actionExit_triggered()\0"
+    "MainWindow\0\0on_actionExit_triggered()\0"
 };
 
 const QMetaObject MainWindow::staticMetaObject = {
@@ -71,11 +69,10 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
         switch (_id) {
-        case 0: on_tabWidget_tabCloseRequested((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 1: on_actionExit_triggered(); break;
+        case 0: on_actionExit_triggered(); break;
         default: ;
         }
-        _id -= 2;
+        _id -= 1;
     }
     return _id;
 }
