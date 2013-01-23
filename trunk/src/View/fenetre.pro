e8618707161eaa0cec97be66db16033daf1df3ca
lefebvreariane@gmail.com <lefebvreariane@gmail.com@e3157d87-c1cc-ae76-3296-7ee3db5a2ebd>
2013-01-23 10:26:15 +0000

    fenetre mise a jour

--- trunk/src/View/fenetre.pro
+++ trunk/src/View/fenetre.pro
@@ -1,22 +1,13 @@
-#-------------------------------------------------
-#
+# -------------------------------------------------
 # Project created by QtCreator 2012-12-10T12:56:30
-#
-#-------------------------------------------------
-
-QT       += opengl core xml
-
+# -------------------------------------------------
+QT += opengl
 TARGET = fenetre
 TEMPLATE = app
-
-
-SOURCES += main.cpp\
-        mainwindow.cpp \
-    sceneparser.cpp
-
-HEADERS  += mainwindow.h \
-    sceneparser.h
-
-FORMS    += mainwindow.ui
-
-CONFIG   += opengl
+SOURCES += main.cpp \
+    mainwindow.cpp \
+    Visualization.cpp
+HEADERS += mainwindow.h \
+    Visualization.h
+FORMS += mainwindow.ui
+CONFIG += opengl
--- trunk/src/View/moc_mainwindow.cpp
+++ trunk/src/View/moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Thu Dec 20 13:50:34 2012
+** Created: Wed Jan 23 10:59:15 2013
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
 **
 ** WARNING! All changes made in this file will be lost!
@@ -23,7 +23,7 @@ static const uint qt_meta_data_MainWindow[] = {
        4,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   14, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -32,12 +32,14 @@ static const uint qt_meta_data_MainWindow[] = {
 
  // slots: signature, parameters, type, tag, flags
       12,   11,   11,   11, 0x08,
+      46,   38,   11,   11, 0x08,
 
        0        // eod
 };
 
 static const char qt_meta_stringdata_MainWindow[] = {
     "MainWindow\0\0on_actionExit_triggered()\0"
+    "checked\0slot_radioButtonToogled(bool)\0"
 };
 
 const QMetaObject MainWindow::staticMetaObject = {
@@ -70,9 +72,10 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_c == QMetaObject::InvokeMetaMethod) {
         switch (_id) {
         case 0: on_actionExit_triggered(); break;
+        case 1: slot_radioButtonToogled((*reinterpret_cast< bool(*)>(_a[1]))); break;
         default: ;
         }
-        _id -= 1;
+        _id -= 2;
     }
     return _id;
 }
