80f0fb71eb91878394f27602d452c06439c4bd6a
coves.pablo@gmail.com <coves.pablo@gmail.com@e3157d87-c1cc-ae76-3296-7ee3db5a2ebd>
2013-03-12 15:16:20 +0000

    parseur en bonne voie

--- trunk/src/Model/realscene.h
+++ trunk/src/Model/realscene.h
@@ -11,39 +11,46 @@
 #include <vector>
 using std::vector;
 using std::pair;
+using std::make_pair;
 using std::cout;
 using std::endl;
 class RealScene : public QObject {
     Q_OBJECT
-    vector<pair<float, float> > _matrice;
-    vector<pair<float, float> > _devetisseur;
-    vector<pair<float, float> > _poincon;
-    vector<pair<float, float> > _tole;
+    vector<pair<float, float> >             _matrice;
+    vector<vector<pair<float, float> > >    _devetisseur;
+    vector<pair<float, float> >             _poincon;
+    vector<pair<float, float> >             _tole;
+    QString uniteTemps;
+    QString uniteDistance;
+    int duree;
+    int axeX;
+    int axeY;
+    int epaisseurTole;
 public:
     // Constructeurs & destructeur
     explicit RealScene(
             vector<pair<float, float> > matrice,
-            vector<pair<float, float> > devetisseur,
+            vector<vector<pair<float, float> > > devetisseur,
             vector<pair<float, float> > poincon,
             vector<pair<float, float> > tole,
             QObject *parent = 0) : QObject(parent), _matrice(matrice), _devetisseur(devetisseur), _poincon(poincon), _tole(tole){}
     explicit RealScene(QString xmlFilePath, QObject *parent=0);
     virtual ~RealScene(){}
     // Modifieurs
-    virtual void addMatrice(pair<float, float> p)       {_matrice.push_back(p);}
-    virtual void addDevetisseur(pair<float, float> p)   {_devetisseur.push_back(p);}
-    virtual void addPoicon(pair<float, float> p)        {_poincon.push_back(p);}
-    virtual void addTole(pair<float, float> p)          {_tole.push_back(p);}
+    virtual void addMatrice(pair<float, float> p)               {_matrice.push_back(p);}
+    virtual void addDevetisseur(int i, pair<float, float> p)    {_devetisseur[i].push_back(p);}
+    virtual void addPoicon(pair<float, float> p)                {_poincon.push_back(p);}
+    virtual void addTole(pair<float, float> p)                  {_tole.push_back(p);}
     // Accesseurs
-    virtual const vector<pair<float, float> > matrice()     const {return _matrice;}
-    virtual const vector<pair<float, float> > devetisseur() const {return _devetisseur;}
-    virtual const vector<pair<float, float> > poincon()     const {return _poincon;}
-    virtual const vector<pair<float, float> > tole()        const {return _tole;}
+    virtual const vector<pair<float, float> > matrice()                 const {return _matrice;}
+    virtual const vector<vector<pair<float, float> > > devetisseur()    const {return _devetisseur;}
+    virtual const vector<pair<float, float> > poincon()                 const {return _poincon;}
+    virtual const vector<pair<float, float> > tole()                    const {return _tole;}
     virtual void resetTole() {_tole.clear();}
 signals:
-    
+
 public slots:
-    
+
 protected:
 
 private:
