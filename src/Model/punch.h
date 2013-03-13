0e77bba386ad050b8533c002cc72a21670666639
coves.pablo@gmail.com <coves.pablo@gmail.com@e3157d87-c1cc-ae76-3296-7ee3db5a2ebd>
2013-03-13 13:17:25 +0000

    Xml avancement

---
+++ src/Model/punch.h
@@ -0,0 +1,18 @@
+#ifndef PUNCH_H
+#define PUNCH_H
+
+#include <QObject>
+
+class punch : public QObject
+{
+    Q_OBJECT
+public:
+    explicit punch(QObject *parent = 0);
+    
+signals:
+    
+public slots:
+    
+};
+
+#endif // PUNCH_H
--- src/Model/scene.h
+++ src/Model/scene.h
@@ -8,12 +8,22 @@
 #include <QtXml/QDomElement>
 #include <QtXml/QDomNode>
 #include <iostream>
+#include <cmath>
 #include <vector>
 using std::vector;
 using std::pair;
 using std::make_pair;
 using std::cout;
 using std::endl;
+
+#define PI atan(1)*4
+#define distanceTemps     (temps, tempsMax, distanceMax)        -distanceMax/2*cos(2*PI*temps/tempsMax)+distanceMax/2
+#define vitesseTemps      (temps, tempsMax, distanceMax)        PI*distanceMax/tempsMax*sin(2*PI*temps/tempsMax)
+#define accelerationTemps (temps, tempsMax, distanceMax)        2*pow(PI,2)*distanceMax/pow(tempsMax,2)*cos(2*PI*temps/tempsMax)
+#define tempsDistance     (distance,     tempsMax, distanceMax) acos((distance-(distanceMax/2))/(-distanceMax/2))/(2*PI/tempsMax)
+#define tempsVitesse      (vitesse,      tempsMax, distanceMax) asin(vitesse*tempsMax/(PI*distanceMax))*tempsMax/(2*PI)
+#define tempsAcceleration (acceleration, tempsMax, distanceMax) acos((acceleration*pow(tempsMax,2))/(2*pow(PI,2)*distanceMax))*tempsMax/(2*PI)
+
 class Scene : public QObject {
     Q_OBJECT
     vector<pair<float, float> >             _matrice;
@@ -22,10 +32,15 @@ class Scene : public QObject {
     vector<pair<float, float> >             _tole;
     QString uniteTemps;
     QString uniteDistance;
-    int duree;
-    int axeX;
-    int axeY;
-    int epaisseurTole;
+    double duree;
+    double axeX;
+    double axeY;
+    double epaisseurTole;
+    double xTranslation;
+    double yTranslation;
+    double xRotation;
+    double yRotation;
+    double angle;
 public:
     // Constructeurs & destructeur
     explicit Scene(
@@ -49,11 +64,11 @@ public:
     virtual void resetTole() {_tole.clear();}
     virtual void save() const;
 private:
-    void fillUnits(QDomElement e);
-    void fillMatrice(QDomElement e);
-    void fillDevetisseur(QDomElement e);
-    void fillPoincon(QDomElement e);
-    void fillTole(QDomElement e);
+    void fillUnits       (QDomElement);
+    void fillMatrice     (QDomElement);
+    void fillDevetisseur (QDomElement);
+    void fillPoincon     (QDomElement);
+    void fillTole        (QDomElement);
 };
 
 #endif // REALSCENE_H
