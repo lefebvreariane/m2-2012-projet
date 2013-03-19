c56cd5e392b85c8c4649d72fccbb0705bfafefc7
lefebvreariane@gmail.com <lefebvreariane@gmail.com@e3157d87-c1cc-ae76-3296-7ee3db5a2ebd>
2013-03-19 11:48:14 +0000

    mise a jour fenetre

--- src/Controler/Visualization.cpp
+++ src/Controler/Visualization.cpp
@@ -2,14 +2,48 @@
 
 #include <ctime>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-Visualization::Visualization(){
-
+Visualization::Visualization(double span, double totalTime, SpanType type)
+    : _span(span),
+      _totalTime(totalTime),
+      _type(type){
+    time_vector();
+    creer_scenes();
 }
 
 Visualization::~Visualization(){
 
 }
 
+void Visualization::creer_scenes(){
+    /*for (unsigned int i=0 ; i<moves.size() ; i++){
+        scenes.push_back();
+    }*/
+}
+
+void Visualization::time_vector(){
+    if (_type == TIME_SPAN){
+        int nbMoves = (int) (_totalTime/_timeSpan);
+        double mod = fmod(_span,_totalSpan);
+        vector<double> timeVector(nbMoves);
+        for(unsigned int i=0 ; i<nbMoves ; i++){
+            timeVector[i] = i*_timeSpan;
+        }
+        if (mod)
+            timeVector[i].push_back(_totalTime);
+    }
+    else{ // SPAN_LENGTH
+        int nbMoves = (int) (_totalTime/_timeSpan);
+        double mod = fmod(_span,_totalSpan);
+        vector<double> timeVector(nbMoves);
+        for(unsigned int i=0 ; i<nbMoves ; i++){
+            timeVector[i] = 1;
+        }
+        if (mod)
+            timeVector[i].push_back(_totalTime);
+    }
+    // appeler les macros pour avoir les déplacements correspondants
+}
--- src/Controler/Visualization.hpp
+++ src/Controler/Visualization.hpp
@@ -1,15 +1,39 @@
 #ifndef VISUALIZATION_HPP
 #define VISUALIZATION_HPP
+#include "Model/scene.h"
+#include <vector>
+
+#define PI atan(1)*4
+#define distanceTemps     (temps, tempsMax, distanceMax)        -distanceMax/2*cos(2*PI*temps/tempsMax)+distanceMax/2
+#define vitesseTemps      (temps, tempsMax, distanceMax)        PI*distanceMax/tempsMax*sin(2*PI*temps/tempsMax)
+#define accelerationTemps (temps, tempsMax, distanceMax)        2*pow(PI,2)*distanceMax/pow(tempsMax,2)*cos(2*PI*temps/tempsMax)
+#define tempsDistance     (distance,     tempsMax, distanceMax) acos((distance-(distanceMax/2))/(-distanceMax/2))/(2*PI/tempsMax)
+#define tempsVitesse      (vitesse,      tempsMax, distanceMax) asin(vitesse*tempsMax/(PI*distanceMax))*tempsMax/(2*PI)
+#define tempsAcceleration (acceleration, tempsMax, distanceMax) acos((acceleration*pow(tempsMax,2))/(2*pow(PI,2)*distanceMax))*tempsMax/(2*PI)
+
+enum SpanType {TIME_SPAN, SPAN_LENGTH};
 
 class Visualization {
 
 public:
 
-    Visualization();
+    Visualization(double span, double totalTime);
     ~Visualization();
 
-//private:
+    double type() const {return _type;}
+    double span() const {return _span;}
+    double total_time() const {return _totalTime;}
+    std::vector<double> time_vector() const {return _timeVector;}
 
+private:
+    std::vector<double> _timeVector;
+    std::vector<Scene> _scenes;
+    SpanType _type;
+    double _span;
+    double _totalTime;
+
+    void creer_scenes();
+    void time_vector();
 //private slots:
 
 };
